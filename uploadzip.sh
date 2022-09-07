6c16c926a0d66e409217ae49cb91fe5b660e9686
5cj <5cj@abnx.dev>
2022-09-07 14:53:51 -0500

    uploadzip.sh macos compatible

--- uploadzip.sh
+++ uploadzip.sh
@@ -1,6 +1,10 @@
 export $(grep -v '^#' .env | xargs)
 GOOS=linux go build -o main main.go
-build-lambda-zip.exe -output main.zip main .env
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  zip main.zip main .env
+else
+  build-lambda-zip.exe -output main.zip main .env
+fi
 aws s3api put-object --bucket $BUCKET_NAME --key lambda/main.zip --body main.zip
 aws lambda update-function-code --function-name $BUCKET_NAME-proxy --s3-bucket $BUCKET_NAME --s3-key lambda/main.zip
 rm main
